1a243ee7138f1e04efcd77ba667a907f889ea762
Akash Kadlag <akashkadlag14@gmail.com>
2024-12-29 17:25:30 +0530

    chore: update prettier config and add vscode settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSpacing: false,
+  arrowParens: "avoid",
 };
